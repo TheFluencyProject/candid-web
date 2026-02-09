ffbba7e72aa6f0663757aadcb0dce2fd64b1dca0
adamvnovak <adamvnovak@gmail.com>
2026-02-09 11:58:54 +0900

    custom redirect links

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -26,6 +26,46 @@ const nextConfig = {
         destination: "https://apps.apple.com/app/id6754859158",
         permanent: true,
       },
+      {
+        source: "/startup",
+        destination: "https://apps.apple.com/app/id6754859158",
+        permanent: true,
+      },
+      {
+        source: "/startups",
+        destination: "https://apps.apple.com/app/id6754859158?ppid=c038ad6e-6323-481e-95fd-2dadeb0bd04d",
+        permanent: true,
+      },
+      {
+        source: "/englishmode",
+        destination: "https://apps.apple.com/app/id6754859158?ppid=ppid=a34b9b94-7d2a-4fac-b792-6e59fb0e5e59",
+        permanent: true,
+      },
+      {
+        source: "/business",
+        destination: "https://apps.apple.com/app/id6754859158?ppid=a5bb8fc4-a398-442f-b401-92c2cc1e050a",
+        permanent: true,
+      },
+      {
+        source: "/news",
+        destination: "https://apps.apple.com/app/id6754859158?ppid=86959fbf-bcba-4bea-829f-5b7d73270854",
+        permanent: true,
+      },
+      {
+        source: "/beauty",
+        destination: "https://apps.apple.com/app/id6754859158",
+        permanent: true,
+      },
+      {
+        source: "/wellness",
+        destination: "https://apps.apple.com/app/id6754859158",
+        permanent: true,
+      },
+      {
+        source: "/basketball",
+        destination: "https://apps.apple.com/app/id6754859158",
+        permanent: true,
+      },
     ];
   },
 };
